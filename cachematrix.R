d0e38e8c18eafef620242bfb893a6978b293312f
rbajr <rackerman2001@gmail.com>
2016-10-21 11:10:55 -0400

    R Programming Assignment 2
    Functions demonstrating lexical scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Assignment 2:  
+## This script contains 2 functions, makeCacheMatrix and cacheSolve
 
-## Write a short comment describing this function
-
+## makeCacheMatrix accepts a matrix (for this exercise the matrix must be square and invertible)
+## and returns a list containing 4 functions which set/get the input matrix or its inversion
 makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL    ##holds the inverted matrix of x
+  
+  ##reinitializes the matrix
+  setMat <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  ##returns the matrix
+  getMat <- function() x
 
+  ##sets the inverted matrix of x
+  setMatInv <- function(invMatrix)  i <<- invMatrix
+  ##returns the inverted matrix of x
+  getMatInv <- function() i
+  ##return a list containing all 4 functions
+  list(setMat = setMat,
+       getMat = getMat,
+       setMatInv = setMatInv,
+       getMatInv = getMatInv
+       )
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve accepts an object of type makeCacheMatrix
+## it returns the inverted matrix by first checking the cache (via the getMatInv function);
+## if the inverted matrix does not exist in cache, the inverted matrix is created, 
+## stored and returned
+cacheSolve <- function(x) {
+  #get the inverted matrix
+  i <- x$getMatInv()
+  if(!is.null(i)) {
+    message("getting inverted matrix from cache")
+    return(i)
+  }
+  #if getMatInv returned NULL, calc the matrix inversion
+  #store it an return
+  data <- x$getMat()
+  i <- solve(data)
+  x$setMatInv(i)
+  i
 }
